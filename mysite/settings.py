0a2fdd547b688e79467c450c281de5efb6d0e281
Ofek Cohen <ofekcohen977@gmail.com>
2023-09-26 14:18:16 +0300

    updating django version again

--- mysite/settings.py
+++ mysite/settings.py
@@ -26,7 +26,6 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 DEBUG = os.environ.get("DEBUG", "False").lower() == "true"
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS").split(" ")
-PYTHON_VERSION = os.environ.get("PYTHON_VERSION")
 
 
 # Application definition
